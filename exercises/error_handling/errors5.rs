9b136cbec4f62920ee5d4c10c3d32d0810ce7d91
deBath <alex.solovov@list.ru>
2023-07-10 19:35:56 +0300

    Complete

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -29,7 +29,7 @@ use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn ???>> {
+fn main() -> Result<(), Box<dyn error::Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
@@ -52,7 +52,7 @@ impl PositiveNonzeroInteger {
         match value {
             x if x < 0 => Err(CreationError::Negative),
             x if x == 0 => Err(CreationError::Zero),
-            x => Ok(PositiveNonzeroInteger(x as u64)),
+            x => Ok(PositiveNonzeroInteger(x as u64))
         }
     }
 }
